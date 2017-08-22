9e8fede6792ec057482b3c33cef4c62f0a8941f1
Jithumon <jithu311990@gmail.com>
2017-08-22 05:33:32 -0400

    Revert "Checking if it's a repository"
    This reverts commit 996f6126ee9788fe6ec0a7cbd9bcaf6fc6defb24.

--- install.sh
+++ install.sh
@@ -100,15 +100,11 @@ case $REPLY in [yY])
 	clear
 	echo -en "${Green}Do you want to use the beta branch? (Y/N): ${Default}"
 	read REPLY
-	if [ -d .git ]; then
-		git stash
-		git checkout beta
-	else
+	if [[ $REPLY == [yY] ]]; then
 		echo -en "${Orange}Fetching latest Group Butler source code\n${Default}"
 		git clone -b beta https://github.com/RememberTheAir/GroupButler.git
 		cd GroupButler
-	fi;
-
+	fi
 	clear
 	echo -en "${BGreen}Group Butler successfully installed! Change values in config file and run ${BRed}./launch.sh${BGreen}.${Default}";;
 	*) echo "Exiting...";;
