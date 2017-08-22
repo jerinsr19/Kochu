938421d5c3769c36db6b55d38b9bc9ab75942c0d
Jithumon <jithu311990@gmail.com>
2017-08-22 05:16:32 -0400

    Revert "Correcting a small error"
    This reverts commit d7b9f6468f0fa89418387c12197da033fbd0569b.

--- install.sh
+++ install.sh
@@ -84,10 +84,8 @@ case $REPLY in [yY])
 	if [ -d .git ]; then
 		echo -en "${Green}Do you want to use the beta branch (If you modified something we will do a stash)? (Y/N): ${Default}"
 		read REPLY
-		if [[ $REPLY == [yY] ]]; then
-			git stash
-			git checkout beta
-		fi
+		git stash
+		git checkout beta
 	fi;
 
 	echo -en "${BGreen}Group Butler successfully installed! Change values in config file and run ${BRed}./launch.sh${BGreen}.${Default}";;
